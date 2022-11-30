50e79e0f97902bb7c3a65850c618d91b640a1034
NiKi Laud3r <119116731+thepirateniki@users.noreply.github.com>
2022-11-30 11:04:20 -0500

    Birthday-Countdown.oy
    This is my first ever python project, which started on November 29th, 2022, and ended on November 30th, 2022. This was inspired by SeanBolt.

--- Birthday-Countdown.py
+++ Birthday-Countdown.py
@@ -1,7 +1,19 @@
 import time
 
-today = date.today
+today = date.today()
 
 birthday = date(2009, 8, 11)
 
 this_years_birthday = date(today.year, birthday.month, birthday.day)
+days_until_birthday = (this_years_birthday-today).days
+days_alive = (today-birthday).days
+
+print('You are ' + str(days_alive) + ' days old!')
+
+if days_until_birthday > 0:
+  print('You have ' + days_until_birthday + 'until your birthday.'
+elif days_until_birthday == 0:
+  print("Happy Birthday!!")
+else:
+  print("Wait until next year for your birthday..")
+        
